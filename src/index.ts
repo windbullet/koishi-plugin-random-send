d5da8ad193c804bca6253394a784ea2031d192ce
windbullet <2536810643@qq.com>
2023-12-15 17:40:36 +0800

    提交文件

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import { Context, Schema, sleep, Random } from 'koishi'
 export const name = 'random-send'
 
 export interface Config {
+  guildId: string[]
   messageList: string[]
   minInterval: number
   maxInterval: number
 }
 
 export const Config: Schema<Config> = Schema.object({
+  guildId:Schema.array(Schema.string())
+    .description("会发送消息的群聊ID，空则所有群聊"),
   messageList:Schema.array(Schema.string())
     .description("随机发送其中的一条消息")
     .required(),
@@ -26,13 +29,20 @@ export function apply(ctx: Context, config: Config) {
     while(true) {
       await sleep(Random.int(config.minInterval * 1000, config.maxInterval * 1000 + 1))
       if (flag) break
-      for (let bot of ctx.bots) {
-        let guilds = []
-        for await (let guild of bot.getGuildIter()) {
-          guilds.push(guild)
+      if (config.guildId.length === 0) {
+        for (let bot of ctx.bots) {
+          let guilds = []
+          for await (let guild of bot.getGuildIter()) {
+            guilds.push(guild)
+          }
+          let guildId = Random.pick(guilds).id
+          await bot.sendMessage(guildId, Random.pick(config.messageList))
         }
-        let guildId = Random.pick(guilds).id
-        await bot.sendMessage(guildId, Random.pick(config.messageList))
+      } else {
+        for (let bot of ctx.bots) {
+          let guildId = Random.pick(config.guildId)
+          await bot.sendMessage(guildId, Random.pick(config.messageList))
+        }
       }
     }
   })
